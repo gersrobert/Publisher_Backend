333317f4ab188be84e6ff0ddcc22784e5960e138
Samuel Branisa <samuel.branisa@pan-net.eu>
2021-03-11 17:26:01 +0100

    debug

--- src/main/java/fiit/mtaa/publisher/controller/UserController.java
+++ src/main/java/fiit/mtaa/publisher/controller/UserController.java
@@ -6,10 +6,12 @@ import fiit.mtaa.publisher.dto.AppUserWithPasswordDTO;
 import fiit.mtaa.publisher.dto.LoginRequestDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -21,25 +23,42 @@ public class UserController extends AbstractController {
 	@PostMapping(value = "/login", headers = "Accept=application/json", produces = "application/json")
 	public ResponseEntity<UUID> login(@RequestBody LoginRequestDTO loginRequestDTO) {
 		throw new RuntimeException("Not yet implemented");
-
 	}
 
 	@GetMapping(value = "/{uuid}")
 	public ResponseEntity<AppUserDTO> getUser(@PathVariable String uuid) {
 		throw new RuntimeException("Not yet implemented");
-
 	}
 
 	@GetMapping(value = "/{uuid}/detailed")
 	public ResponseEntity<AppUserDetailedDTO> getUserDetailed(@PathVariable String uuid) {
 		throw new RuntimeException("Not yet implemented");
-
 	}
 
 	@PostMapping(value = "/register")
 	public ResponseEntity registerUser(@RequestBody AppUserWithPasswordDTO user) {
-		throw new RuntimeException("Not yet implemented");
+		// boolean response = true;
 
+		// System.out.println(user.getPasswordHash());
+		
+		// // try {
+		// // 	response = userService.registerAppUser(user);
+		// // } catch (Exception e) {
+		// // 	logger.error("Error registering user", e);
+		// // 	throw new InternalServerException(e);
+		// // }
+
+		// if (response) {
+		// 	return new ResponseEntity(HttpStatus.CREATED);
+		// } else {
+		// 	return new ResponseEntity(HttpStatus.FORBIDDEN);
+		// }
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	@PostMapping(value = "/set_photo")
+	public ResponseEntity setPhoto(@RequestBody Byte[] photo) {
+		throw new RuntimeException("Not yet implemented");
 	}
 
 	@GetMapping(value = "/{userId}/actions/{articleId}")
@@ -47,4 +66,15 @@ public class UserController extends AbstractController {
 		throw new RuntimeException("Not yet implemented");
 
 	}
+
+	// @GetMapping(value = "/test")
+	// public ResponseEntity<AppUserDTO> getTest() {
+	// 	AppUserDTO response = new AppUserDTO();
+	// 	response.setFirstName("firstName");
+	// 	response.setLastName("lastName");
+	// 	response.setUserName("userName");
+	// 	response.setId("id");
+
+	// 	return ResponseEntity.of(Optional.of(response));
+	// }
 }
